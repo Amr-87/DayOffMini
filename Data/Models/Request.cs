31154da655e0788a40958a900ddc1d420f35339f
Amr <amr.aboulela97@gmail.com>
2025-12-30 16:19:25 +0200

    models created, migration to mysql done .. empty controllers created

---
+++ Data/Models/Request.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DayOffMini.Data.Models
+{
+    public class Request
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int StatusId { get; set; }
+        public int EmployeeId { get; set; }
+        public int LeaveTypeId { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string? Reason { get; set; }
+
+        public virtual Employee Employee { get; set; } = null!;
+        public virtual LeaveType LeaveType { get; set; } = null!;
+        public virtual RequestStatus Status { get; set; } = null!;
+
+
+    }
+}
