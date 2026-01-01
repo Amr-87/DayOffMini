e8e14900c39236245096036717ae985c5c18b782
Amr <amr.aboulela97@gmail.com>
2026-01-01 14:27:33 +0200

    crud for roles done

---
+++ Repositories/Generic/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿
+using DayOffMini.Data.DbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace DayOffMini.Repositories.Generic
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<T>();
+
+        }
+        public async Task CreateAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int entityId)
+        {
+            var entity = await _dbSet.FindAsync(entityId);
+            _dbSet.Remove(entity!);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<ICollection<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int entityId)
+        {
+            return await _dbSet.FindAsync(entityId);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
