38078f36ff05c8ef15ebd4565092b8a40e73bf7a
Amr <amr.aboulela97@gmail.com>
2026-01-01 17:47:37 +0200

    removing user and roles tables and initializing other features dtos and interfaces

---
+++ Services/Interfaces/ILeaveRequestService.cs
@@ -0,0 +1,15 @@
+﻿using DayOffMini.Controllers.DTOs;
+
+namespace DayOffMini.Services.Interfaces
+{
+    public interface ILeaveRequestService
+    {
+        Task CreateAsync(LeaveRequestDto leaveRequest);
+        Task UpdateAsync(LeaveRequestDto leaveRequest);
+        Task<LeaveRequestDto?> GetByIdAsync(int leaveRequestId);
+        Task<ICollection<LeaveRequestDto>> GetAllAsync();
+        Task DeleteAsync(int leaveRequestId);
+
+        Task SaveChangesAsync();
+    }
+}
